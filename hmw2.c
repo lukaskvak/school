9cb9b7baf3ceeb1483f0f6a2a5c8552f62379c02
Lukas Kvak <81817156+lukaskvak@users.noreply.github.com>
2021-12-06 09:05:32 -0800

    Add files via upload

---
+++ hmw2.c
@@ -0,0 +1,45 @@
+
+/*
+Write a C program to print reverse Pyramid or reverse equilateral triangle made of asterisks.
+   Example:
+   
+   Enter the height: 5
+   
+   *********
+    *******
+     *****
+      ***
+       * 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+int i,j;
+int height;
+printf("please enter the height: \n");
+scanf("%d",&height);
+printf("\n================================\n");
+         
+        for(i=1;i<=height;i++)    {
+            
+            for(j=1;j<i;j++)    {
+                    printf(" ");}                   
+            
+            for(j=i;j<=height;j++){
+                    printf("*");  }
+            
+            for(j=i;j<height;j++){
+                    printf("*"); }
+                                 
+        printf("\n");
+                                      }
+
+
+printf("\n================================");
+
+
+
+    return 0;
+}
